b2b4c7d5542a387300caceda4e50e35af1e0a27d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-02 00:57:04 -0300

    Saved __author__.py at Timestamp: 2018-05-02 00:57:02

--- __author__.py
+++ __author__.py
@@ -1,2 +1,3 @@
 {
 "ada": {"author_nick": "carlo", "author_name": "Carlo Emmanoel", "author_email": "carlo@nce.ufrj.br", "author_org": "LABASE", "author_site": "http://labase.superpython.net", "author_public": "on"},
+"henrietta": {"author_nick": "fono-games", "author_name": "Games Inteligentes e Fonoaudiologia Computacional", "author_email": "labasence@gmail.com", "author_org": "LABASE-FONOCOMP", "author_site": "http://www.superpython.net", "author_public": "on"},
