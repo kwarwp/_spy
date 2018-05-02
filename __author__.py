c6c3c45e21248cf72b5f78ad24de8dcb6568b922
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-01 23:22:56 -0300

    fix for json

--- __author__.py
+++ __author__.py
@@ -1,2 +1,2 @@
-[
-{"ada": {"author_nick": "carlo", "author_name": "Carlo Emmanoel", "author_email": "carlo@nce.ufrj.br", "author_org": "LABASE", "author_site": "http://labase.superpython.net", "author_public": "on"}},
+{
+"ada": {"author_nick": "carlo", "author_name": "Carlo Emmanoel", "author_email": "carlo@nce.ufrj.br", "author_org": "LABASE", "author_site": "http://labase.superpython.net", "author_public": "on"},
