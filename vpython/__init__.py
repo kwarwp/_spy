da77d92e6d5996e655cf1f519490f790d042b66f
Carlo E. T. Oliveira <carlo@nce.ufrj.br>
2018-05-29 21:41:14 -0300

    Spike do Vpython funcionando no vitollino; falta integrar

--- vpython/__init__.py
+++ vpython/__init__.py
@@ -1,41 +1,37 @@
 from browser import document, window
-from javascript import JSObject, JSConstructor
 
+GLOWSCRIPT = window.glowscript
+
+from .primitive import box, arrow, cone, curve, pyramid, helix, cylinder
+from .primitive import sphere, ring, attach_trail, compound
 from .utils import create_script_tag
-from .primitive import box, arrow, cone, curve, pyramid, helix, cylinder, sphere, attach_trail, compound
-
 from .vector import vec
+import os
 
-version = "1.0"
+project_stdlib = "../stlib/"  # os.path.dirname("../stdlib{}/".format(os.path.abspath(__file__)))
 
-create_script_tag('../../labase/glow/jslibs/jquery-1.6.2.min.js')
-create_script_tag('../../labase/glow/jslibs/glow.1.0.min.js')
+version = "2.7"
 
-#find a better way to do this..
-while 1:
-  _glowscript=JSObject(glowscript)
+create_script_tag(project_stdlib + 'glow.2.7.min.js')
 
-  try:
-    if _glowscript.version == version:
-       break
-  except TypeError:
-    pass
 
-#or we could make this some type of function instead of a class
-class glow:
-  def __init__(self, container):
-      self._id=document.get(id=container)[0]
-      setattr(self._id, 'id', '')
+class Glow:
+    def __init__(self, container):
+        self._id = document.get(id=container)[0]
+        setattr(self._id, 'id', '')
 
-      setattr(window, '__context', {})
-      setattr(getattr(window, '__context'), 'glowscript_container', 
-              self._id.elt)
-  
-#todo, make canvas its own class    
+        setattr(window, '__context', {})
+        setattr(getattr(window, '__context'), 'glowscript_container',
+                self._id.elt)
+
+
+# todo, make canvas its own class
 def canvas():
-    return _glowscript.canvas()
+    return window.glowscript.canvas()
+
 
 def rate(t, func):
-    _glowscript.rate(t, func)
+    window.glowscript.rate(t, func)
 
-color=_glowscript.color
+
+color = window.glowscript.color
--- vpython/utils.py
+++ vpython/utils.py
@@ -1,11 +1,13 @@
-from browser import document
 import urllib.request
 
-def create_script_tag(src):
-    _fp, _, _=urllib.request.urlopen(src)
-    _data=_fp.read()
+from browser import document
 
-    _tag=document.createElement('script')
-    _tag.type="text/javascript"
-    _tag.text=_data
+
+def create_script_tag(src):
+    _fp = urllib.request.urlopen(src)
+    _data = _fp.read()
+
+    _tag = document.createElement('script')
+    _tag.type = "text/javascript"
+    _tag.text = _data
     document.get(tag='head')[0].appendChild(_tag)
