cab884904e9ac209dcafe98af3330fabdb02c3c4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-01 10:12:27 -0300

    added html

--- vpython/main.py
+++ vpython/main.py
@@ -1,4 +1,4 @@
-from browser import document, window
+from browser import document, window, html
 from .primitive import box, arrow, cone, curve, pyramid, helix, cylinder, ellipsoid
 from .primitive import sphere, ring, attach_trail, compound, extrusion, text
 from .utils import create_script_tag
