71cc4eab01bc5f64cb3c0b27f64d747a4d1e182c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-01 07:43:21 -0300

    try fix spg reload

--- vpython/main.py
+++ vpython/main.py
@@ -3,19 +3,23 @@ from .primitive import box, arrow, cone, curve, pyramid, helix, cylinder, ellips
 from .primitive import sphere, ring, attach_trail, compound, extrusion, text
 from .utils import create_script_tag
 from .vector import vec
-import os
 
+GLOWID = '_glow_'
 project_stdlib = "../stlib/"  # os.path.dirname("../stdlib{}/".format(os.path.abspath(__file__)))
 
 version = "2.7"
 
-create_script_tag('/js/glow.2.7.min.js')
+create_script_tag(project_stdlib + 'glow.2.7.min.js')
 
 
 class Glow:
     def __init__(self, container):
-        self._id = document.get(id=container)[0]
-        setattr(self._id, 'id', '')
+        glowid = GLOWID+container
+        if glowid in document:
+            document[container].remove()
+        document[container] <= html.DIV(Id=glowid, Class="glowscript")
+        self._id = document.get(id=glowid)[0]
+        # setattr(self._id, 'id', '')
 
         setattr(window, '__context', {})
         setattr(getattr(window, '__context'), 'glowscript_container',
