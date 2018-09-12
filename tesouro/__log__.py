24e095fc1a7b927b81ca1adfb6e8d6b887450fec
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-12 10:47:19 -0300

    Saved tesouro/__log__.py at Timestamp: 2018-09-12 13:47:16

--- tesouro/__log__.py
+++ tesouro/__log__.py
@@ -127,3 +127,25 @@ KeyError: aranha
     return Sprite(SPRITES[face], index=0, tit=tit)
 KeyError: 1
 '''},
+{'date': 'Wed Sep 12 2018 10:43:18.406 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 215
+    main(gui=GUI).inicia()
+  module tesouro.tesouro line 352
+    self.mesa.inicia()
+  module tesouro.tesouro line 280
+    Jogo.GUI.set_timeout(self.rodada, 2000)
+  module <module> line 166
+    timer.set_timeout(cls, rodada, param)
+TypeError: set_timeout() takes 2 positional arguments but more were given
+'''},
