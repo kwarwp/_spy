88f9dd836c0da5c63b1b4652f976ab5348716b06
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-12 10:35:57 -0300

    Saved tesouro/__log__.py at Timestamp: 2018-09-12 13:35:56

--- tesouro/__log__.py
+++ tesouro/__log__.py
@@ -43,3 +43,25 @@ NameError: name 'ModuleNotFoundError' is not defined
     self.admite = Jogo.GUI.admite
 AttributeError: 'Gui' object has no attribute 'admite'
 '''},
+{'date': 'Wed Sep 12 2018 10:31:59.143 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 215
+    main(gui=GUI).inicia()
+  module <module> line 209
+    return Jogo(gui=gui, jogadores=[JogadorSimples(None, jogador) for jogador in jogadores])
+  module <module> line 196
+    self._inicia(nome)
+  module <module> line 203
+    except (ModuleNotFoundError, TypeError):
+NameError: name 'ModuleNotFoundError' is not defined
+'''},
