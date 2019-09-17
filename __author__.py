d92d8950db509d92884aebe171a63e446f4ddf6e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-17 20:23:28 -0300

    Saved __author__.py @2019-09-17 20:23

--- __author__.py
+++ __author__.py
@@ -4,3 +4,4 @@
 "henrietta": {"author_nick": "fono-games", "author_name": "Games Inteligentes e Fonoaudiologia Computacional", "author_email": "labasence@gmail.com", "author_org": "LABASE-FONOCOMP", "author_site": "http://www.superpython.net", "author_public": "on"},
 "soraya": {"author_nick": "carlo", "author_name": "Carlo Oliveira", "author_email": "carlo@nce.ufrj.br", "author_org": "LABASE", "author_site": "", "author_public": "on"},
 "henrietta": {"author_nick": "brunafiuza", "author_name": "Bruna Fiuza", "author_email": "brunafiuza@live.com", "author_org": "", "author_site": "", "author_public": "on"},
+"margaret": {"author_nick": "fonocomp1_19", "author_name": "FonoComp I 2019", "author_email": "carlo@nce.ufrj.br", "author_org": "UFRJ", "author_site": "http://labase.nce.ufrj.br", "author_public": "on"},
