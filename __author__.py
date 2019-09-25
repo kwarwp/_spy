dc2cab9871443a2c5b0a51421951a4f701220ce2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-25 05:05:58 -0300

    Saved __author__.py @2019-09-25 08:05

--- __author__.py
+++ __author__.py
@@ -7,3 +7,4 @@
 "margaret": {"author_nick": "fonocomp1_19", "author_name": "FonoComp I 2019", "author_email": "carlo@nce.ufrj.br", "author_org": "UFRJ", "author_site": "http://labase.nce.ufrj.br", "author_public": "on"},
 "mary_shaw": {"author_nick": "POO PPGI 19.3", "author_name": "Labase - Disciplina de ProgramaÃ§Ã£o Orientada a Objetos - PPGI 2019-3", "author_email": "carlo@ufrj.br", "author_org": "LABASE", "author_site": "labase.nce.ufrj.br", "author_public": "on"},
 "vera": {"author_nick": "fonocomp_II_19", "author_name": "Fonocomp II 2019", "author_email": "hello@ub.br", "author_org": "", "author_site": "", "author_public": "on"},
+"vera": {"author_nick": "fonocomp_II_19", "author_name": "Fonocomp II 2019", "author_email": "hello@ub.br", "author_org": "LABASE", "author_site": "labase.nce.ufrj.br", "author_public": "on"},
