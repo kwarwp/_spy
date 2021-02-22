d871acdc1e402d2e423bdf7a57b060a490561950
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-02-22 08:49:13 -0300

    Saved __author__.py @2021-02-22 11:49

--- __author__.py
+++ __author__.py
@@ -19,3 +19,4 @@
 "roberta": {"author_nick": "Python_2020", "author_name": "Curso Iniciação Python 2020", "author_email": "carlo@nce.ufrj.br", "author_org": "LABASE", "author_site": "labase.nce.ufrj.br", "author_public": "on"},
 "phyllis": {"author_nick": "WorkShop_2020", "author_name": "Workshop SupyGirls Outubro 20202", "author_email": "carlo@nce.ufrj.br", "author_org": "LABASE", "author_site": "labase.nce.ufrj.br", "author_public": "on"},
 "elizabeth": {"author_nick": "WorkShop_2020", "author_name": "Workshop SupyGirls Outubro 20202", "author_email": "carlo@nce.ufrj.br", "author_org": "LABASE", "author_site": "labase.nce.ufrj.br", "author_public": "on"},
+"joanna": {"author_nick": "bellexand", "author_name": "isabelle alexandre da silva", "author_email": "isabellesilva031@gmail.com", "author_org": "", "author_site": "", "author_public": "on"},
