bf5e34871f33a20898f18b0851420edee7623782
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2021-07-05 11:16:21 -0300

    Fix megan

--- __author__.py
+++ __author__.py
@@ -20,6 +20,5 @@
 "phyllis": {"author_nick": "WorkShop_2020", "author_name": "Workshop SupyGirls Outubro 20202", "author_email": "carlo@nce.ufrj.br", "author_org": "LABASE", "author_site": "labase.nce.ufrj.br", "author_public": "on"},
 "elizabeth": {"author_nick": "WorkShop_2020", "author_name": "Workshop SupyGirls Outubro 20202", "author_email": "carlo@nce.ufrj.br", "author_org": "LABASE", "author_site": "labase.nce.ufrj.br", "author_public": "on"},
 "joanna": {"author_nick": "bellexand", "author_name": "isabelle alexandre da silva", "author_email": "isabellesilva031@gmail.com", "author_org": "", "author_site": "", "author_public": "on"},
-"joanna": {"author_nick": "bellexand", "author_name": "isabelle alexandre da silva", "author_email": "isabellesilva031@gmail.com", "author_org": "", "author_site": "", "author_public": "on"},
 "grete": {"author_nick": "dfssf", "author_name": "sdfd", "author_email": "akakaka@gmail.com", "author_org": "", "author_site": "", "author_public": "on"},
-"megan": {"author_nick": "katia", "author_name": "Katia GuimarÃ£es SOusa Palomo", "author_email": "profa.katia.palomo@gmail.com", "author_org": "Instituto Federal de BrasÃ\xadlia", "author_site": "www.ifb.edu.br", "author_public": "on"},
+"megan": {"author_nick": "katia", "author_name": "Katia Guimarães Sousa Palomo", "author_email": "profa.katia.palomo@gmail.com", "author_org": "Instituto Federal de Brasília", "author_site": "www.ifb.edu.br", "author_public": "on"},
