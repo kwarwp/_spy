7e6064049e3c4933f092ca6221ed714a329a8cd7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2022-06-06 13:40:10 -0300

    Incorporate Roteiro

--- vitollino/main.py
+++ vitollino/main.py
@@ -1730,6 +1730,102 @@ class Bloco:
         self.monta()
         self.monta = self.nao_monta
         # self.centro.norte.vai()
+	
+	
+from collections import namedtuple
+Ator = namedtuple('Elenco','ator nome mini alinha')
+Fala = namedtuple('Fala','ator fala prox age')  # , defaults=(None,)*4)
+A = namedtuple('Ali','e m d')(-1, 0, 1)
+
+
+class Roteiro:
+    def __init__(self, cena, roteiro, elenco=(), foi=None):
+        """Cria um roteiro de falas entre diversos personagens.
+        Os parâmetros roteiro e elenco são definidos por tupla nomeadas.
+            Ator
+                ator:
+                    instância de elemento do ator que no diálogo.
+                nome:
+                    nome que vai aparecer no título do ator.
+                mini:
+                    fração da altura do ator na miniatura (0.1 a 1.0)
+                alinha:
+        :param cena: cena onde o diálogo acontece.
+        :param roteiro: um conjunto de falas entre os personagens.
+        :param elenco: caracterização dos personagens.
+        :param foi: número de cubos na horizontal.
+        """
+        self.dic_ator = {a.ator: a for a in elenco}
+        _prox = zip(roteiro, roteiro[1:] + [Fala(None, "", None, None)])
+        self.foi = foi if foi else lambda *_: None
+        roteiro = [Fala(a, f, g if g else (p.ator if p else None), x) for [a, f, g, x], p in _prox]
+        self.elenco, self.roteiro = elenco, roteiro
+        self._foi = lambda *_: None
+        script = self
+        for _ator in elenco:
+            _ator.ator.vai = self.nada
+            _ator.ator.tit = _ator.nome
+            _ator.ator.elt.style.filter = "brightness(30%)"
+        protagonista = elenco[0].ator if elenco else roteiro[0].ator
+        self.atores = [ator.ator for ator in elenco] if elenco else [ator.ator for ator in roteiro]
+        protagonista.vai = self.segue
+        protagonista.elt.style.filter = "brightness(100%)"
+
+        class Falar(Texto):
+            def __init__(self, ator, fala, prox, act=None, mini=1, **kwarg):
+                self.ator, self.fala, self.prox = ator, fala, prox
+                self._foi = act or self.nada
+                minih = 80 / mini
+                self.mini = Elemento(ator.img, cena=cena, w=80, h=80, tipo=f"80px {minih}px",
+                                     style=dict(top="20%", margin="-10px 10%"))
+                super().__init__(cena, fala, **kwarg)
+
+            def esconde(self, *_):
+                self.mini.elt.remove()
+                self.ator.elt.style.filter = "brightness(30%)"
+                script.testa(self.prox)
+                # script.segue()
+                # super().esconde()
+                self._foi()
+
+            def vai(self, *_):
+                super().vai()
+                self.ator.elt.style.filter = "brightness(5%)"
+                self.ator.vai = self.nada
+
+            @property
+            def foi(self):
+                return self._foi
+
+            @foi.setter
+            def foi(self, value):
+                self._foi = value
+
+            def nada(self, *_):
+                pass
+
+        self._fala = Falar
+
+    def nada(self, *_):
+        pass
+
+    def segue(self, *_):
+        ator, fala, prox, action = self.scripter()
+        # ator.elt.style.filter = "brightness(30%)"
+        fala = self._fala(ator, fala, prox, action, mini=self.dic_ator[ator].mini)  # .vai()
+        if prox:
+            prox.vai = self.segue
+        fala.vai()
+
+    def testa(self, prox, *_):
+        if self.roteiro:
+            prox.elt.style.filter = "brightness(100%)"
+        else:
+            for ato in self.atores:
+                ato.elt.style.filter = "brightness(100%)"
+
+    def scripter(self, *_):
+        return self.roteiro.pop(0)
 
 
 class Jogo:
@@ -1752,7 +1848,7 @@ class Jogo:
         self.html = html
         self.window = win
         self.timer = timer
-        from _spy.vitollino.jogos import Roteiro
+        # from _spy.vitollino.jogos import Roteiro
 	self.rt = self.roteiro = Roteiro
 
 
